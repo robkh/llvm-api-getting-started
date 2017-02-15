6aa0f2c0328007fb040abac25fffddfd531e0bb9
Robert Khasanov <robert.khasanov@tu-dresden.de>
2017-02-15 20:40:31 +0100

    Added function.

--- sum.cpp
+++ sum.cpp
@@ -3,8 +3,12 @@
 
 int main(int argc, char**argv) {
 
-  llvm::LLVMContext Context;
-  llvm::Module* Mod = new llvm::Module("my_module", Context);
+  llvm::LLVMContext Ctx;
+  llvm::Module* Mod = new llvm::Module("my_module", Ctx);
+
+  llvm::Constant* Func = Mod->getOrInsertFunction("sum",
+      llvm::Type::getInt32Ty(Ctx), llvm::Type::getInt32Ty(Ctx),
+      llvm::Type::getInt32Ty(Ctx), NULL);
 
   delete Mod;
   return 0;
