7a8fee9ee01138017e2a58ab391a53b9c1890462
Robert Khasanov <robert.khasanov@tu-dresden.de>
2017-02-15 20:53:29 +0100

    Added basic block. LLVM namespace added.

--- sum.cpp
+++ sum.cpp
@@ -1,14 +1,24 @@
 #include "llvm/IR/Module.h"
 #include "llvm/IR/LLVMContext.h"
+using namespace llvm;
 
 int main(int argc, char**argv) {
 
-  llvm::LLVMContext Ctx;
-  llvm::Module* Mod = new llvm::Module("my_module", Ctx);
+  LLVMContext Ctx;
 
-  llvm::Constant* Func = Mod->getOrInsertFunction("sum",
-      llvm::Type::getInt32Ty(Ctx), llvm::Type::getInt32Ty(Ctx),
-      llvm::Type::getInt32Ty(Ctx), NULL);
+  // Create module
+  Module* Mod = new Module("my_module", Ctx);
+
+  // Create function
+  Constant* Con = Mod->getOrInsertFunction("sum",
+      Type::getInt32Ty(Ctx), Type::getInt32Ty(Ctx),
+      Type::getInt32Ty(Ctx), NULL);
+
+  Function* Sum = cast<Function>(Con);
+  Sum->setCallingConv(llvm::CallingConv::C);
+
+  // Create BB
+  BasicBlock* block = BasicBlock::Create(Ctx, "entry", Sum);
 
   delete Mod;
   return 0;
